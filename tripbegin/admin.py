573c136ec6a69c64a06394ed71b5da678e884992
Saif <kamruljaman@Kamruls-MacBook-Pro.local>
2022-09-21 02:31:48 +0600

    added all database model in admin panel

--- tripbegin/admin.py
+++ tripbegin/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
-from .models import User, Location
+from .models import *
 
 
-# Register your models here.
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-	list_display = ['username', 'email']
+# # Register your models here.
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+# 	list_display = ['username', 'email']
+#
+#
+# @admin.register(Location)
+# class LocationAdmin(admin.ModelAdmin):
+# 	list_display = ['city', 'region']
 
 
-@admin.register(Location)
-class LocationAdmin(admin.ModelAdmin):
-	list_display = ['city', 'region']
+# register model admin
+admin.site.register(User, admin.ModelAdmin)
+admin.site.register(Location, admin.ModelAdmin)
+admin.site.register(History, admin.ModelAdmin)
+admin.site.register(Flight, admin.ModelAdmin)
+admin.site.register(Hotel, admin.ModelAdmin)
+admin.site.register(Payment, admin.ModelAdmin)
